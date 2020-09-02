a63cc2e1b47b65d55bf07d69832de7facdc0c2bd
SeverinLaicher1996 <42353002+SeverinLaicher1996@users.noreply.github.com>
2020-09-02 12:15:11 +0200

    Update import.py

--- import.py
+++ import.py
@@ -14,3 +14,11 @@ os.system("mv LSCDetection-master/modules/utils_.py utils_.py")
 
 
 
+
+os.system("wget https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_eng.zip")
+
+os.system("unzip semeval2020_ulscd_eng.zip")
+os.system("rm semeval2020_ulscd_eng.zip")
+
+os.system("mv semeval2020_ulscd_eng/corpus1/lemma/ccoha1.txt.gz Data/ccoha1.txt.gz")
+os.system("mv semeval2020_ulscd_eng/corpus2/lemma/ccoha2.txt.gz Data/ccoha1.txt.gz")
