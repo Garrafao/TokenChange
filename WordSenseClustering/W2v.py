ed189272f13ea657278b9cb565fe285388ecbe1e
SeverinLaicher1996 <42353002+SeverinLaicher1996@users.noreply.github.com>
2020-09-07 12:16:20 +0200

    Update W2v.py

--- WordSenseClustering/W2v.py
+++ WordSenseClustering/W2v.py
@@ -28,24 +28,20 @@ def main():
     args = docopt("""
 
     Usage:
-        W2v.py  <pathTestSentences> <pathw2i> <outPathVectors> <windowSize2> <pathCorpus>
+        W2v.py  <pathTestSentences> <outPathVectors> <windowSize2> 
         
     Arguments:
        
         <pathTestSentences> = Path to the test sentences
-        <pathw2i> = <pathw2i>
         <outPathVectors> = Path for storing the vectors 
         <windowSize2> = Window size (20 works good)
-        <pathCorpus> = path to the corpus 
     
     """)
     
   
     pathTestSentences = args['<pathTestSentences>']
-    pathw2i = args['<pathw2i>']
     outPathVectors = args['<outPathVectors>']
     windowSize2 = int(args['<windowSize2>'])
-    pathCorpus = args['<pathCorpus>']
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.CRITICAL)
     print("")
@@ -61,23 +57,6 @@ def main():
 
     #Load w2i
     w2i = np.load(pathw2i, allow_pickle='TRUE').item()
-      
-    #Calculate IDF for every word 
-    docFreq={}
-    for i in range(0, len(w2i)):
-        docFreq[i]=0
-    with gzip.open(pathCorpus,'rt', encoding="utf-8") as sentences: 
-        count=0
-        try:
-            for sentence in sentences:
-                count=count+1
-                for word in set(sentence.split()):
-                    if word in w2i:
-                        docFreq[w2i[word]]+=1
-        except:
-            pass
-        for key, value in w2i.items(): 
-            docFreq[value] = math.log10(count/max(docFreq[value],1)) 
 
     #Load TestSentences 
     contextVectorList=[]
@@ -89,29 +68,33 @@ def main():
     
     #Calculate contextVectorMatrix
     logging.critical("Calculate contextVectorMatrix") 
+
     nonExisting=False
-    target=str(testSentences[0]["original_word"])        
-    for dic in testSentences:
-        sentence = dic['sentence'].split()
-        for i, word in enumerate(sentence):  
-            if str(i) == dic['target_index'] and word == target:
-                toMelt=[]
+    #self.target=str(self.testSentences[0]["original_word"])        
+    for dic in self.testSentences:
+        self.sentence = dic['sentence_token'].split()
+        for i, word in enumerate(self.sentence):  
+            if str(i) == dic['target_index']:
+
+                self.toMelt=[]
                 toMeltIDF=[]
-                lowerWindowSize = max(i-windowSize2, 0)
-                upperWindowSize = min(i+windowSize2, len(sentence))
-                window = sentence[lowerWindowSize:i] + sentence[i+1:upperWindowSize+1] 
-                if word in w2i:
-                    windex = w2i[word]
-                    for contextWord in window:
-                        if contextWord in w2i:
-                            if contextWord in model.wv.vocab:
-                                if contextWord != "$":
-                                    contextWordIndex = w2i[contextWord]
-                                    toMelt.append(preprocessing.normalize([model.wv[contextWord]], norm='l2')[0]
-                                                  *math.pow(docFreq[contextWordIndex],1 ))  
-                    contextVectorList.append(getContextVector(toMelt))  
+                self.lowerWindowSize = max(i-self.windowSize, 0)
+                self.upperWindowSize = min(i+self.windowSize, len(self.sentence))
+                self.window = self.sentence[self.lowerWindowSize:i] + self.sentence[i+1:self.upperWindowSize+1] 
+                if word in model.wv.vocab:
+                    for contextWord in self.window:
+                        if contextWord in model.wv.vocab:
+                            if contextWord != "$":
+                                self.toMelt.append(preprocessing.normalize([model.wv[contextWord]], norm='l2')[0]) 
+
+
+                    self.contextVectorList.append(self.getContextVector(self.toMelt))  
                 else:
-                    nonExisting=True
+                    self.contextVectorList.append(np.zeros(300))
+
+
+    
+
 
 
     #Normalize vectors in length
